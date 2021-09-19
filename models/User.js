0f9ade3dd299ddb308877cdd589fbfa7b7698115
raych2 <rtongco@gmail.com>
2021-09-18 21:52:00 -0700

    Update User model to include passwordConfirmation key

--- models/User.js
+++ models/User.js
@@ -1,22 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
-const UserSchema = new Schema(
-  {
-    firstName: {type: String, required: true, maxLength: 100},
-    lastName: {type: String, required: true, maxLength: 100},
-    username: {type: String, required: true, maxLength: 50},
-    password: {type: String, required: true, maxLength: 50},
-    membershipStatus: {type: String, required: true, maxLength: 20},
-    admin: {type: Boolean, required: true},
-  }
-);
+const UserSchema = new Schema({
+  firstName: { type: String, required: true, maxLength: 100 },
+  lastName: { type: String, required: true, maxLength: 100 },
+  username: { type: String, required: true, maxLength: 50 },
+  password: { type: String, required: true, maxLength: 50 },
+  passwordConfirmation: { type: String, required: true, maxLength: 50 },
+  membershipStatus: { type: String, maxLength: 20 },
+  admin: { type: Boolean },
+})
 
-UserSchema
-.virtual('fullName')
-.get(function () {
-  return this.firstName + ' ' + this.lastName;
-});
+UserSchema.virtual('fullName').get(function () {
+  return this.firstName + ' ' + this.lastName
+})
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema)
