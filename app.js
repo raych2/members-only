4806092b3c23460825ab8c2da0f47b5e50da957b
raych2 <rtongco@gmail.com>
2021-09-21 20:31:41 -0700

    Update prettier config file

--- app.js
+++ app.js
@@ -1,99 +1,99 @@
-const createError = require('http-errors')
-const express = require('express')
-const path = require('path')
-const dotenv = require('dotenv').config()
-const session = require('express-session')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcryptjs')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 
 //Set up mongoose connection
-const mongoose = require('mongoose')
-const mongoDB = process.env.MONGO_URI
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGO_URI;
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const app = express()
+const app = express();
 
 passport.use(
   new LocalStrategy((username, password, done) => {
     User.findOne({ username: username }, (err, user) => {
       if (err) {
-        return done(err)
+        return done(err);
       }
       if (!user) {
-        return done(null, false, { message: 'Incorrect username' })
+        return done(null, false, { message: 'Incorrect username' });
       }
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
-          return done(null, user)
+          return done(null, user);
         } else {
-          return done(null, false, { message: 'Incorrect password' })
+          return done(null, false, { message: 'Incorrect password' });
         }
-      })
-      return done(null, user)
-    })
+      });
+      return done(null, user);
+    });
   })
-)
+);
 
 passport.serializeUser(function (user, done) {
-  done(null, user.id)
-})
+  done(null, user.id);
+});
 
 passport.deserializeUser(function (id, done) {
   User.findById(id, function (err, user) {
-    done(err, user)
-  })
-})
+    done(err, user);
+  });
+});
 
 app.use(function (req, res, next) {
-  res.locals.currentUser = req.user
-  next()
-})
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger('dev'))
-app.use(express.json())
+app.use(logger('dev'));
+app.use(express.json());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
-)
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-module.exports = app
+module.exports = app;
